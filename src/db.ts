eab6eb9526d4aad6fc31fcad6ad659c4e463b32b
silva-kleyton <silva-kleyton@hotmail.com>
2021-06-15 00:50:12 -0300

    ajust mongoUrl db

--- src/db.ts
+++ src/db.ts
@@ -1,12 +1,15 @@
 import mongoose from "mongoose";
 import "dotenv/config";
+import verifyEnvironment from "./utils/verifyEnviorement";
 
-const { DB_PORT, DB_HOST, DB_USER, DB_PASS, DB_DATABASE } = process.env
+const { DB_PORT, DB_HOST, DB_USER, DB_PASS, DB_DATABASE, MONGO_URL } = process.env;
 
-console.log(`mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}?authSource=admin`)
+const mongoUrl = verifyEnvironment() === "local" ?
+    `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}?authSource=admin` :
+    `${MONGO_URL}`
 
 mongoose.set("useFindAndModify", false);
-mongoose.connect(`mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}?authSource=admin`, {
+mongoose.connect(mongoUrl, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
